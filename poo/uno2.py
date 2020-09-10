781134636a01fc3e524d4e8932fb7b722c295c5c
Javier Garay <javier.garay@2brains.cl>
2020-09-10 13:58:16 -0300

    revolver las cartas

--- poo/uno2.py
+++ poo/uno2.py
@@ -1,3 +1,4 @@
+import random
 """
 Generate the uno deck of 108 cards.
 Parameters: None
@@ -20,8 +21,20 @@ def buildDeck():
     for i in range(4):
         deck.append(wilds[0])
         deck.append(wilds[1])
-
-    print(deck)
+    #print(deck)
     return deck
 
-buildDeck()
+"""
+Shuffles a list of items passed into it
+Parameters: deck -> list
+Return values: deck -> list
+"""
+def shuffleDeck(deck):
+    for cardPos in range(len(deck)):
+        randPos = random.randint(0,107)
+        deck[cardPos], deck[randPos] = deck[randPos], deck[cardPos]
+    return deck
+
+unoDeck = buildDeck()
+unoDeck = shuffleDeck(unoDeck)
+print(unoDeck)
